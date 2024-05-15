b0a5c70137561eb5d21064dbfe34ef9d47d9e58d
ViktorGulis <142294827+ViktorGulis@users.noreply.github.com>
2024-05-16 01:00:01 +0300

    16.5.
    homepage graph works

--- bap/lib/screens/home/home_screen.dart
+++ bap/lib/screens/home/home_screen.dart
@@ -2,29 +2,100 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:fl_chart/fl_chart.dart'; // Import the line chart widget
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _getCurrentUsername(),
-      builder: (context, AsyncSnapshot<String> snapshot) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          FutureBuilder(
+            future: _getCurrentUsername(),
+            builder: (context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // While the username is being fetched, display a loading indicator
+                return Center(child: CircularProgressIndicator());
+              } else {
+                if (snapshot.hasError) {
+                  // If an error occurs while fetching the username, display an error message
+                  return Center(child: Text('Error fetching username'));
+                } else {
+                  // If the username is fetched successfully, display it
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Hello ${snapshot.data}!',
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 32,
+                      ),
+                    ),
+                  );
+                }
+              }
+            },
+          ),
+          SizedBox(height: 10), // Add some spacing between text and chart
+          Expanded(
+            child: _buildLineChart(), // Use a method to build the line chart
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLineChart() {
+    return FutureBuilder<List<FlSpot>>(
+      future: _getWorkoutData(),
+      builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          // While the username is being fetched, display a loading indicator
           return Center(child: CircularProgressIndicator());
         } else {
           if (snapshot.hasError) {
-            // If an error occurs while fetching the username, display an error message
-            return Center(child: Text('Error fetching username'));
+            return Center(child: Text('Error loading data'));
           } else {
-            // If the username is fetched successfully, display it
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Hello ${snapshot.data}!',
-                style: GoogleFonts.bebasNeue(
-                  fontSize: 32,
+            return AspectRatio(
+              aspectRatio: 2.0,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  right: 16,
+                  left: 16,
+                  top: 16,
+                  bottom: 240,
+                ),
+                child: LineChart(
+                  LineChartData(
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: snapshot.data!,
+                        barWidth: 8,
+                        isCurved: true,
+                        curveSmoothness: 0.25,
+                        preventCurveOverShooting: true,
+                        isStrokeCapRound: true,
+                        dotData: const FlDotData(show: false),
+                      ),
+                    ],
+                    titlesData: const FlTitlesData(
+                      show: true,
+                      leftTitles: AxisTitles(
+                        axisNameWidget: Text('weight'),
+                        axisNameSize: 25,
+                      ),
+                      bottomTitles: AxisTitles(
+                        axisNameWidget: Text('workouts'),
+                        axisNameSize: 25,
+                      ),
+                      rightTitles: AxisTitles(
+                        axisNameWidget: Text('weight'),
+                        axisNameSize: 25,
+                      ),
+                      topTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             );
@@ -34,37 +105,57 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
+  Future<List<FlSpot>> _getWorkoutData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      try {
+        QuerySnapshot workoutsSnapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .collection('workouts')
+            .orderBy('timestamp', descending: true)
+            .limit(15)
+            .get();
+
+        List<FlSpot> spots = [];
+        int index = 0;
+
+        // Reverse the order of documents to display recent data on the right side
+        List<QueryDocumentSnapshot> reversedWorkouts =
+            workoutsSnapshot.docs.reversed.toList();
+
+        for (QueryDocumentSnapshot workout in reversedWorkouts) {
+          // Assuming the field name in Firestore is 'totalWeight'
+          double totalWeight = workout.get('totalWeight');
+          spots.add(FlSpot(index.toDouble(), totalWeight));
+          index++;
+        }
+        return spots;
+      } catch (e) {
+        print('Error fetching workout data: $e');
+        return [];
+      }
+    } else {
+      return [];
+    }
+  }
 
   Future<String> _getCurrentUsername() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      // If the user is authenticated, fetch their username from Firestore
       try {
         DocumentSnapshot userDoc = await FirebaseFirestore.instance
             .collection('users')
             .doc(user.uid)
             .get();
-
-
-        // Get the username from the document snapshot
         String username = userDoc.get('username');
-
-
-        // Return the fetched username
         return username;
       } catch (e) {
-        // If there's an error while fetching the username, return a default username
         print('Error fetching username: $e');
         return 'User';
       }
     } else {
-      // If the user is not authenticated, return a default username
       return 'User';
     }
   }
 }
-
-
-
-
-
